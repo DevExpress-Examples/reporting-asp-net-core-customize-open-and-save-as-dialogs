9ea858bfa426efa5264bad1fd63185d1c78268f2
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2022-12-13 19:24:29 +0400

    auto sync for version 2022.2

---
+++ CS/AspNetCoreReportingDialogCustomization/Services/CustomReportStorageWebExtension.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AspNetCoreReportingDialogCustomization.PredefinedReports;
+using DevExpress.XtraReports.UI;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AspNetCoreReportingDialogCustomization.Services {
+    public class CustomReportStorageWebExtension : DevExpress.XtraReports.Web.Extensions.ReportStorageWebExtension {
+        readonly string ReportDirectory;
+        const string FileExtension = ".repx";
+        public CustomReportStorageWebExtension(IWebHostEnvironment env) {
+            ReportDirectory = Path.Combine(env.ContentRootPath, "Reports");
+            if(!Directory.Exists(ReportDirectory)) {
+                Directory.CreateDirectory(ReportDirectory);
+            }
+        }
+
+        private bool IsWithinReportsFolder(string url, string folder) {
+            var rootDirectory = new DirectoryInfo(folder);
+            var fileInfo = new FileInfo(Path.Combine(folder, url));
+            return fileInfo.Directory.FullName.ToLower().StartsWith(rootDirectory.FullName.ToLower());
+        }
+
+        public override bool CanSetData(string url) {
+            // Determines whether or not it is possible to store a report by a given URL. 
+            // For instance, make the CanSetData method return false for reports that should be read-only in your storage. 
+            // This method is called only for valid URLs (i.e., if the IsValidUrl method returned true) before the SetData method is called.
+
+            return true;
+        }
+
+        public override bool IsValidUrl(string url) {
+            // Determines whether or not the URL passed to the current Report Storage is valid. 
+            // For instance, implement your own logic to prohibit URLs that contain white spaces or some other special characters. 
+            // This method is called before the CanSetData and GetData methods.
+
+            var rootDirectory = new DirectoryInfo(ReportDirectory);
+            var fileInfo = new FileInfo(Path.Combine(ReportDirectory, url));
+            return fileInfo.Directory.FullName.ToLower().StartsWith(rootDirectory.FullName.ToLower());
+        }
+
+        public override byte[] GetData(string url) {
+            // Returns report layout data stored in a Report Storage using the specified URL. 
+            // This method is called only for valid URLs after the IsValidUrl method is called.
+            try {
+                var filename = url.Split('\\').Last();
+                var fullPath = Path.Combine(ReportDirectory, url + FileExtension);
+                if(File.Exists(fullPath)) {
+                    return File.ReadAllBytes(fullPath);
+                }
+                if(ReportsFactory.Reports.ContainsKey(url)) {
+                    using(MemoryStream ms = new MemoryStream()) {
+                        ReportsFactory.Reports[url]().SaveLayoutToXml(ms);
+                        return ms.ToArray();
+                    }
+                }
+            } catch(Exception ex) {
+                throw new DevExpress.XtraReports.Web.ClientControls.FaultException("Could not get report data.", ex);
+            }
+            throw new DevExpress.XtraReports.Web.ClientControls.FaultException(string.Format("Could not find report '{0}'.", url));
+        }
+
+        public override Dictionary<string, string> GetUrls() {
+            // Returns a dictionary of the existing report URLs and display names. 
+            // This method is called when running the Report Designer, 
+            // before the Open Report and Save Report dialogs are shown and after a new report is saved to a storage.
+
+            var repxFiles = new DirectoryInfo(ReportDirectory).GetFiles("*" + FileExtension, SearchOption.AllDirectories);
+            var dictionary = repxFiles
+                .Select(x => {
+                    var directory = x.Directory.FullName == ReportDirectory ? "" : x.Directory.FullName.Substring(ReportDirectory.Length + 1);
+                    return Path.Combine(directory, Path.GetFileNameWithoutExtension(x.Name));
+                })
+                .ToDictionary(k => k, v => v);
+            foreach(var predefinedReportList in ReportsFactory.Reports)
+                if(!dictionary.ContainsKey(predefinedReportList.Key))
+                    dictionary.Add(predefinedReportList.Key, predefinedReportList.Key);
+            return dictionary;
+        }
+
+        public override void SetData(XtraReport report, string url) {
+            // Stores the specified report to a Report Storage using the specified URL. 
+            // This method is called only after the IsValidUrl and CanSetData methods are called.
+            if(!IsWithinReportsFolder(url, ReportDirectory))
+                throw new DevExpress.XtraReports.Web.ClientControls.FaultException("Invalid report name.");
+
+            var parts = url.Split('\\');
+            var newDirectory = ReportDirectory + "\\" + parts[0];
+            if(parts.Length == 2 && !Directory.Exists(newDirectory)) {
+                Directory.CreateDirectory(newDirectory);
+            }
+
+            report.SaveLayoutToXml(Path.Combine(ReportDirectory, url + FileExtension));
+        }
+
+        public override string SetNewData(XtraReport report, string defaultUrl) {
+            // Stores the specified report using a new URL. 
+            // The IsValidUrl and CanSetData methods are never called before this method. 
+            // You can validate and correct the specified URL directly in the SetNewData method implementation 
+            // and return the resulting URL used to save a report in your storage.
+            SetData(report, defaultUrl);
+            return defaultUrl;
+        }
+    }
+}
--- CS/Reporting-How-To-Customize-Open-And-Save-As-Dialogs/AspNetCoreReportingDialogCustomization/Controllers/HomeController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace AspNetCoreReportingDialogCustomization.Controllers {
-    public class HomeController : Controller {
-        public IActionResult Index() {
-            return View();
-        }
-    }
-}
